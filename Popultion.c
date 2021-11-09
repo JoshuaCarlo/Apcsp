731a527dbea6c555e098d9cc7a89a494e4b890e8
JoshuaCarlo <93368812+JoshuaCarlo@users.noreply.github.com>
2021-11-09 21:25:40 +0300

    Update Popultion.c

--- Popultion.c
+++ Popultion.c
@@ -1,24 +1,16 @@
-#include <studio.h>
 #include <cs50.h>
-/*
-    Joshua, this is not the code required for population. If you recall the first iteration
-    of population should take a startin population size n, and then return the size of n
-    after 1 year, using the algorithms provided in the assignment description. Let me
-    know if you have questions.
-*/
-int main(void){
+#include <stdio.h>
 
-    int userNum = get_int("enter a number: ");
-    int sum = userNum + 900;
-    int diff = userNum - 5;
-    int product = userNum * 7;
-    int quotient = userNum / 12;
-    int mod = userNum % 99;
+int main(void)
+{
+
+    int u = get_int("enter a number: ");
+ 
+    int b = u / 3;
+    int d = u / 4;
+    int t = u + b - d;
     
     
-    printf("sum: %i\n",sum);
-    printf("difference: %i\n",diff);
-    printf("product: %i\n",product);
-    printf("quotient: %i\n",quotient);
-    printf("mod: %i\n",mod);
+    printf("Total llama population: %i\n", t);
+
 }
