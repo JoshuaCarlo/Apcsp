cfa97306e18b085f2072fd795840b79537dc5737
Daniel Ramirez <67478253+DRamirezGAAQ@users.noreply.github.com>
2021-11-09 10:35:49 +0300

    Update Popultion.c

--- Popultion.c
+++ Popultion.c
@@ -1,6 +1,11 @@
 #include <studio.h>
 #include <cs50.h>
-
+/*
+    Joshua, this is not the code required for population. If you recall the first iteration
+    of population should take a startin population size n, and then return the size of n
+    after 1 year, using the algorithms provided in the assignment description. Let me
+    know if you have questions.
+*/
 int main(void){
 
     int userNum = get_int("enter a number: ");
