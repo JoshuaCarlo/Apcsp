0c581bc0dc1cde20ace32519fecc92e6786b14d6
Daniel Ramirez <67478253+DRamirezGAAQ@users.noreply.github.com>
2021-11-10 06:42:17 +0300

    Update Popultion.c

--- Popultion.c
+++ Popultion.c
@@ -1,6 +1,13 @@
 #include <cs50.h>
 #include <stdio.h>
 
+/*
+This code uses consistent style and give correct output. The variable names that you have used
+are not up to the C programming style guides. Variable names should make the data stored within them immediatly 
+understood by someone reading the code. Single letter variable names do not allow this to happen. Instead of 'b' use 'born', etc.
+Overall a solid start to C programming.
+*/
+
 int main(void)
 {
 
